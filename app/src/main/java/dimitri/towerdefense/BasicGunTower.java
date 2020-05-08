b9bf5d26ae33f7eb101a055d94fe1bb48db5e4bf
ruthc <ruthvikchowdary@csus.edu>
2020-05-07 20:27:23 -0700

    Merge branch 'classic' of https://github.com/DimitriosPt/AndroidTowerDefense into classic
    # Conflicts:
    #	app/src/main/java/dimitri/towerdefense/BasicAOETower.java

---
+++ app/src/main/java/dimitri/towerdefense/BasicGunTower.java
@@ -0,0 +1,46 @@
+package dimitri.towerdefense;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Point;
+
+import java.util.List;
+
+public class BasicGunTower extends Tower {
+    AttackStrategy attackStrategy = new GunStrategy();
+    public BasicGunTower(){
+        Context newContext = TowerDefense.getContext();
+        new BitmapFactory();
+        Bitmap unscaledBitmap = BitmapFactory.decodeResource(newContext.getResources(), R.drawable.basic_tower);
+        Bitmap scaledBitmap = Bitmap
+                .createScaledBitmap(unscaledBitmap,
+                        200, 200, false);
+        this.setObjectBitmap(scaledBitmap);
+
+        this.setLocation(new Point(100,50));
+        this.setRange(200);
+        this.setDamage(5);
+        this.setAttackSpeed(100);
+    }
+
+    @Override
+    void attack(List<Enemy> enemies)
+    {
+        attackStrategy.attack(this, enemies);
+    }
+
+    @Override
+    void spawn(Point location)
+    {
+        this.setLocation(location);
+    }
+
+    @Override
+    void draw(Canvas canvas, Paint paint)
+    {
+        canvas.drawBitmap(this.getObjectBitmap(), this.getLocation().x, this.getLocation().y, paint);
+    }
+}
