74dc50eadc4ec10bf6a6b2524ab5c5cb35d6a7c6
ruthc <ruthvikchowdary@csus.edu>
2020-05-12 18:13:16 -0700

    Set there hp

---
+++ app/src/main/java/dimitri/towerdefense/Base.java
@@ -0,0 +1,17 @@
+package dimitri.towerdefense;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PointF;
+
+public class Base extends StaticGameObject {
+    @Override
+    void spawn(PointF location) {
+
+    }
+
+    @Override
+    void draw(Canvas canvas, Paint paint) {
+
+    }
+}
--- app/src/main/java/dimitri/towerdefense/BasicAOETower.java
+++ app/src/main/java/dimitri/towerdefense/BasicAOETower.java
@@ -23,7 +23,7 @@ public class BasicAOETower extends Tower  {
 
         this.setLocation(new PointF(100,50));
         this.setRange(600);
-        this.setDamage(3);
+        this.setDamage(9);
         this.setAttackSpeed(700);
         this.attackStrategy = new AreaOfEffectAttackStrategy();
     }
